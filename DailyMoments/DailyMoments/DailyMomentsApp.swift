87ba1e7673abfe6df2e6b10868cae95355b38a3c
Geonwoo Lee <2dubu.dev@gmail.com>
2024-10-17 16:18:57 +0900

    🎉 :: tada

---
+++ DailyMoments/DailyMoments/DailyMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyMomentsApp.swift
+//  DailyMoments
+//
+//  Created by 이건우 on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct DailyMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
